7474b6ca85c5bc50d1695d7b5595d3af32a8b7a8
David Sapiro <dsap48@gmail.com>
2024-02-20 15:04:19 -0800

    added completed SerialGenerator class

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Creates Serial Generator beginning at start"""
+        self.start = start
+        self.count = -1
+
+    def __repr__(self):
+        return f"""Serial Generator with start of {self.start}
+            and current count of {self.start + self.count}"""
+
+    def reset(self):
+        """Sets count back to original start value"""
+        self.count = -1
+
+    def generate(self):
+        """Increments count by 1 and returns the new count value."""
+        self.count += 1
+        return self.count + self.start
