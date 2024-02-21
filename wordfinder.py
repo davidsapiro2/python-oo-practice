a2cb6ed996967b4818091d82c442fecc4baf1e99
andres-ordonezs <andresmw.89@gmail.com>
2024-02-20 20:35:14 -0500

    Subclass done

--- wordfinder.py
+++ wordfinder.py
@@ -20,7 +20,7 @@ class WordFinder:
         """ Reads a file given in the intance's location,
         and returns a list with every single word in the file"""
         word_list = []
-
+# TODO: Use docstrip instead
         with open(self.location, "r") as file:
             for line in file:
                 word_list.append(line.rstrip())
@@ -37,13 +37,9 @@ class WordFinder:
 
 class SpecialWordFinder(WordFinder):
     """Subclass of WordFinder that omits blank lines and comment lines"""
+# TODO: __init__ not needed since there are no additional properties
 
-    def __init__(self, location):
-        super().__init__(location)
-        self.remove_special_words()
-
-
-    def remove_special_words(self):
-        for word in self.word_list:
-            if (word.startswith("#") or len(word) == 0):
-                self.word_list.remove(word)
+    def get_words(self):
+        """ Reads a file given in the intance's location,
+        and returns a list with every single word in the file"""
+        return [word for word in super().get_words() if not (word.startswith("#") or word == '')]
